fa5c51804e9edc84b9b4cbf0e23c7d70baa4f0b1
Vojtech Spurny <spurny2@gmail.com>
2025-07-30 14:46:27 +0200

    initial cleanup

---
+++ test_launch.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/bash
+ros2 launch mrs_uav_gazebo_simulation mrs_drone_spawner.launch.py custom_config:=/home/mrs/devel_workspace/src/mrs_uav_gazebo_simulation/ros_packages/mrs_uav_gazebo_simulation/config/spawner_custom_config.yaml
