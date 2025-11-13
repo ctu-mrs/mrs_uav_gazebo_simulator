b11a7b8ac1ff7bad25a6dfe10d9fc6cd5c89f923
Vojtech Spurny <spurny2@gmail.com>
2025-11-13 08:31:51 +0100

    remove installing of tests folder

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import find_packages, setup
 
 package_name = 'mrs_uav_gazebo_simulator'
-data_directories = ['launch', 'config', 'models', 'ROMFS', 'tmux', 'tests']
+data_directories = ['launch', 'config', 'models', 'ROMFS', 'tmux']
 
 data_files_list = []
 
